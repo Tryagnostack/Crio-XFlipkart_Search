baeb7481fd3c26baaf82ff505141096f8e916a2e
Vishnukrishnan M <vishkrishman@gmail.com>
2024-08-15 15:35:10 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,15 +1,25 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -17,6 +27,7 @@ import java.util.logging.Level;
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+
 
 public class TestCases {
     ChromeDriver driver;
@@ -51,6 +62,70 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+    }
+
+    @Test (enabled = true)
+    public void testCase01() throws InterruptedException{
+        Wrappers.initTest(driver);
+        // WebElement search = driver.findElement(By.className("Pke_EE"));
+        // System.out.println(search);
+        Wrappers.SearchWrapper("Washing Machine",driver);
+
+        WebElement popularity = driver.findElement(By.xpath("//*[@class='zg-M3Z'][text()='Popularity']"));
+        popularity.click();
+
+        Thread.sleep(500);
+
+        List<WebElement> ratings = driver.findElements(By.xpath("//*[@class='XQDdHH']"));
+        for(WebElement rating : ratings){
+            String val = Wrappers.ScrollandPrint(driver, rating);
+            if(Float.parseFloat(val) <= 4)
+                System.out.println(val);
+        }
+        // Thread.sleep(1000);
+    }
+
+    @Test(enabled = true)
+    public void testCase02() throws InterruptedException{
+        Wrappers.initTest(driver);
+        Wrappers.SearchWrapper("iPhone", driver);
+
+        List<WebElement> discounts = driver.findElements(By.xpath("//*[@class='UkUFwK']"));
+        for(WebElement discount : discounts){
+            String text = Wrappers.ScrollandPrint(driver, discount);
+            String val = text.substring(0,text.length()-5);
+            if(Integer.parseInt(val) > 17){
+                WebElement ele = discount.findElement(By.xpath("./ancestor::div[contains(@class,'BfVC2z')]/preceding-sibling::*/div[1]"));
+                System.out.println(ele.getText());
+                System.out.println(text);
+            }
+        }
+
+        // Thread.sleep(1000);
+    }
+
+    @Test (enabled = true)
+    public void testCase03() throws InterruptedException{
+        Wrappers.initTest(driver);
+
+        Wrappers.SearchWrapper("Coffee Mug", driver);
+        driver.findElement(By.xpath("//*[@class='_6i1qKy'][contains(text(),'4')]")).click();
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10L)).ignoring(NoSuchElementException.class);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("_6tw8ju")));
+        
+        List<WebElement> cards = driver.findElements(By.xpath("//div[@class='slAVV4' and not(descendant::div[@class='xgS27m'])]"));
+        for(int i = 0; i<5; i++){
+            WebElement card = cards.get(i);
+            String title = card.findElement(By.xpath("./a[@class='wjcEIp']")).getText();
+            String image_URL = card.findElement(By.xpath("./a[@class='VJA3rP']")).getAttribute("href");
+            System.out.println(title);
+            System.out.println(image_URL);
+            System.out.println("------------");
+        }
+        
+        Thread.sleep(1000);
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -7,10 +7,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.swing.Action;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -19,4 +22,22 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static void SearchWrapper(String text, WebDriver driver){
+        WebElement search = driver.findElement(By.className("Pke_EE"));
+        search.sendKeys(text);
+        Actions act = new Actions(driver);
+        act.sendKeys(Keys.ENTER).perform();
+    }
+
+    public static String ScrollandPrint(WebDriver driver, WebElement ele){
+        Actions act = new Actions(driver);
+
+        act.scrollToElement(ele).perform();
+        String rate = ele.getText();
+        return rate;
+    }
+
+    public static void initTest(WebDriver driver){
+        driver.get("https://www.flipkart.com");
+    }
 }
